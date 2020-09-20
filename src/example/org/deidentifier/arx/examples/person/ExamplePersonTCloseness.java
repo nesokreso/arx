19cadc26e1d35b47138b4d783559b5150c38fe59
Nenad Jevdjenic <nenad.jevdjenic@gmail.com>
2020-09-20 21:59:20 +0200

    Improve examples

--- src/example/org/deidentifier/arx/examples/person/ExamplePersonTCloseness.java
+++ src/example/org/deidentifier/arx/examples/person/ExamplePersonTCloseness.java
@@ -17,20 +17,17 @@
 
 package org.deidentifier.arx.examples.person;
 
-import org.deidentifier.arx.ARXConfiguration;
 import org.deidentifier.arx.AttributeType;
 import org.deidentifier.arx.Data;
 import org.deidentifier.arx.AttributeType.Hierarchy.DefaultHierarchy;
 import org.deidentifier.arx.criteria.HierarchicalDistanceTCloseness;
-import org.deidentifier.arx.criteria.KAnonymity;
-import org.deidentifier.arx.metric.Metric;
 
 /**
  * This class represents an example for person data anonymized with the T-Closeness privacy model.
  * 
  * @author Nenad Jevdjenic
  */
-public class ExamplePersonTCloseness extends ExamplePerson {
+public class ExamplePersonTCloseness extends ExamplePersonKAnonymity {
 	/**
 	 * Entry point.
 	 */
@@ -38,18 +35,15 @@ public class ExamplePersonTCloseness extends ExamplePerson {
 		try {
 			Data data = csvInit26AttrLarge();
 			data = setInsensitiveAttr(data);
-			data = setQuasiIdentifiersString(data);
+			data = setQuasiIdentifiers(data);
+			setKAnonymity();
 	        DefaultHierarchy countryHierarchy = createHierarchyCountry(data, COUNTRY_OF_ORIGIN);
 	        DefaultHierarchy nationHierarchy = createHierarchyCountry(data, NATIONALITY);
 	        data.getDefinition().setAttributeType(COUNTRY_OF_ORIGIN, AttributeType.SENSITIVE_ATTRIBUTE);
 			data.getDefinition().setAttributeType(NATIONALITY, AttributeType.SENSITIVE_ATTRIBUTE);
-	        
-			config = ARXConfiguration.create();
-			config.addPrivacyModel(new KAnonymity(2));
-			config.setSuppressionLimit(1d);
-			config.setQualityModel(Metric.createEntropyMetric());
 	        config.addPrivacyModel(new HierarchicalDistanceTCloseness(COUNTRY_OF_ORIGIN, 0.6d, countryHierarchy));
 	        config.addPrivacyModel(new HierarchicalDistanceTCloseness(NATIONALITY, 0.6d, nationHierarchy));
+	        
 	        runAnonymization(data);
 		} catch (Exception e) {
 			System.out.println(e);
